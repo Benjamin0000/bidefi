ee57a8a72010a165a895cc4286bc5008388534ed
Home <you@example.com>
2023-06-27 12:56:22 +0100

    made a change

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,8 +39,8 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
-    wsHost: window.location.hostname,
-    wsPort: 6001,
+    wsHost: window.location.hostname+'/ws',
+    wsPort: 443,
     forceTLS: false,
     disableStats: true,
 });
