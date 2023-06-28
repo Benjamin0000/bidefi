1bda3502de825cf2e71fb2ec6eb9a6bb136b9b97
Home <you@example.com>
2023-06-28 01:36:02 +0100

    changed contract address

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -173,7 +173,7 @@ function getWinner($id)
     $contract = new Contract(env('NODE_ENDPOINT'), get_abi());
     $result =  ""; 
     try{
-        $contract->at('0x3733104daE61D6dA13aF964449bB41F9d6C5FE3c')->call('items', $id, function($error, $data) use(&$result){
+        $contract->at('0x0c94C4d8Cd13CD9dD75282F991e4fc4B4263cCfB')->call('items', $id, function($error, $data) use(&$result){
             $result = $data;
         });
     }catch(\Exception $e){
