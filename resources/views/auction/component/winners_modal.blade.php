2a7f3b5ce3e7a4556e62840a78163053bad749f1
Home <you@example.com>
2023-10-18 07:30:03 +0100

    made a change

---
+++ resources/views/auction/component/winners_modal.blade.php
@@ -0,0 +1,16 @@
+        
+<div class="modal fade popup" id="winners" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered" role="document">
+        <div class="modal-content">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            <div class="modal-body space-y-20 pd-40">
+                <h3>Winners</h3>
+                <div>
+                    
+                </div> 
+            </div>
+        </div>
+    </div>
+</div> 
--- resources/views/bidder.blade.php
+++ resources/views/bidder.blade.php
@@ -1,19 +1,31 @@
 @if($item->status > 0)
-<div class="avatar">
-    @if( $avatar = $item->get_last_bidder_avatar()  )
-        <img src="{{Storage::url($avatar)}}" alt="">
+    @if($item->status > 1 && $item->share > 1)
     @else 
-        <img src="/assets/images/avatar/avt-3.jpg" alt="">
+        <div class="avatar">
+            @if( $avatar = $item->get_last_bidder_avatar()  )
+                <img src="{{Storage::url($avatar)}}" alt="">
+            @else 
+                <img src="/assets/images/avatar/avt-3.jpg" alt="">
+            @endif 
+        </div>
     @endif 
-</div>
 @endif 
 <div class="info">
     @if($item->status == 1)
         <span>Last Bidder</span>
     @elseif($item->status > 1)
-        <span>Won By</span>
+        @if($item->share <= 1)
+            <span>Won By</span>
+        @endif 
     @endif 
+
     @if($item->status > 0)
-        <h6><a href="{{route('auction.show', $item->id)}}">{{get_bidder($item->bidder_id)}}</a> </h6>
-    @endif 
-</div> 
+        @if($item->status > 1 && $item->share > 1)
+            <button data-toggle="modal" data-target="#winners" class="btn btn-primary btn-sm">View Winners</button>
+        @else 
+            <h6><a href="{{route('auction.show', $item->id)}}">{{get_bidder($item->bidder_id)}}</a> </h6>
+        @endif 
+    @endif
+</div>
+
+@include('auction.component.winners_modal')
