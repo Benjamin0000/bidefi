fcb34536e6fa1292c3cd73f7849bd45d31f24a0b
Home <you@example.com>
2023-10-18 08:26:07 +0100

    made a change

--- resources/views/auction/component/winners_modal.blade.php
+++ resources/views/auction/component/winners_modal.blade.php
@@ -17,9 +17,9 @@
                                     <div class="author-avatar">
                                         <a href="#">
                                             @if($avatar = $winner->user->avatar)
-                                                <img width="30" src="{{Storage::url($avatar)}}" alt="" class="avatar">
+                                                <img style="width:30px !important" src="{{Storage::url($avatar)}}" alt="" class="avatar">
                                             @else 
-                                                <img width="30" src="/assets/images/avatar/avt-2.jpg" alt="" class="avatar">
+                                                <img style="width:30px !important" src="/assets/images/avatar/avt-2.jpg" alt="" class="avatar">
                                             @endif 
                                         </a>
                                         <div class="badge"></div>
