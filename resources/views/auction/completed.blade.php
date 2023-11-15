ade62b49317be151c0b2ce31e9d9bd90f52ac3d8
Home <you@example.com>
2023-11-15 08:54:09 +0100

    made a change

--- resources/views/auction/completed.blade.php
+++ resources/views/auction/completed.blade.php
@@ -5,7 +5,7 @@
         <div class="row" id="item_con">
             @include('auction.component.item', ['auctions'=>$items])
         </div>
-        @if($items->count() == 8)
+        @if($items->count() == 12)
         <div class="text-center">
             <button class="btn btn-primary" data-id='3' id="llmore">Load more</button>
         </div> 
